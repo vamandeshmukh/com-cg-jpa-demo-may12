1adb644ca3127c53d8e347c857ea5e3fa16e4e15
vamandeshmukh <vamandeshmukh@gmail.com>
2022-06-10 12:39:13 +0530

    JPA

--- src/main/java/com/cg/jpa/demo/App.java
+++ src/main/java/com/cg/jpa/demo/App.java
@@ -1,13 +1,15 @@
 package com.cg.jpa.demo;
 
 /**
- * Hello world!
+ * 
+ * @author Vaman Deshmukh
+ *         {@link https://github.com/vamandeshmukh/com-cg-jpa-demo-may12}
+ * @apiNote JPA and Hibernate demo project for CG trainees 12 May 2022 batch 2
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
